dcab41fcbcc9515cf42061f44e536e798a3532f5
Zahra Mohamed <zahra520@hotmail.com>
2021-01-20 23:27:02 -0500

    add Interviewer Items & List

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+}
+
+export default function InterviewerList(props){
+  const interviewers = props.interviewers.map(interviewer => {
+    return ( 
+    <InterviewerListItem
+      key ={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.value}
+      onChange={(e) => props.onChange(interviewer.id)}
+      />)
+    })
+  return (
+    <section className="interviewers">
+    <h4 className="interviewers__header text--light">Interviewer</h4>
+    <ul className="interviewers__list">
+      {interviewers}
+    </ul>
+    </section>
+  )
+};
