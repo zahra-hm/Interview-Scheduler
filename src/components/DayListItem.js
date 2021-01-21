f7fde00fcc2307119ae2b4b7cc721b387507c9df
Zahra Mohamed <zahra520@hotmail.com>
2021-01-20 23:23:03 -0500

    fixed typo

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,7 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-  const dayClass = classnames("day-list__items", {
+  const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
@@ -21,9 +21,9 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className = {dayClass} onClick={() => props.setDay(props.name)}>
+    <li className= {dayClass} onClick={() => props.setDay(props.name)} >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
