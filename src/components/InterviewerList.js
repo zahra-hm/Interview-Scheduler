0ab5766b67ca96655ab6b5ee46caab696e304005
Zahra Mohamed <zahra520@hotmail.com>
2021-01-27 14:00:11 -0500

    initial commit; add prop types to interviewerList, first test

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -18,7 +18,7 @@ export default function InterviewerList(props) {
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.value}
-      setInterviewer={(event) => props.onChange(interviewer.id)}
+      onChange={(event) => props.onChange(interviewer.id)}
       />)
     })
   return (
---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,35 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+import React from "react";
+
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render, cleanup } from "@testing-library/react";
+
+/*
+  We import the component that we are testing
+*/
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  it("renders without student name if not provided", () => {
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
